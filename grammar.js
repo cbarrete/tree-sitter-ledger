d5d0d579801b5298ca3622888674f69efe3e41bc
Ivan Tham <pickfire@riseup.net>
2022-06-22 23:09:31 +0800

    Add comment and tag

--- grammar.js
+++ grammar.js
@@ -165,16 +165,22 @@ module.exports = grammar({
             // TODO $.automated_xact,
         ),
 
-        // TODO note opt
         plain_xact: $ => createXact($,
             seq($.date,
                 optional(seq($.whitespace, $.status)),
                 optional(seq($.whitespace, $.code)),
                 optional(seq($.whitespace, $.payee)),
-                '\n'),
+                choice($.note, '\n')
             ),
+        ),
 
-        periodic_xact: $ => createXact($, seq('~', $.whitespace, $.interval, '\n')),
+        periodic_xact: $ => createXact($,
+            seq('~',
+                $.whitespace,
+                $.interval,
+                choice($.note, '\n')
+            ),
+        ),
 
         interval: $ => {
             ci = s => new RegExp(caseInsensitive(s))
@@ -222,7 +228,10 @@ module.exports = grammar({
 
         payee: $ => /[^(*!\n][^*!\n]*/,
 
-        note: $ => seq(';', /.*/),
+        note: $ => seq(';', repeat($.tag), /.*/),
+
+        // TODO not sure why this is broken
+        tag: $ => seq(optional(':'), /[^\s:]+/, ':'),
 
         posting: $ => seq(
             $.whitespace,
