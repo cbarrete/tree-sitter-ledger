186428ebfcef60949fe373bbf09d786130e8f011
Alexis Hildebrandt <afh@surryhill.net>
2023-02-09 11:11:35 +0100

    Add highlight captures for timeclock

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -2,11 +2,19 @@
 ((note) @comment.note)
 
 ((date) @number.date)
+((time) @number.time)
 ((interval) @number.interval)
 ((quantity) @number.quantity)
 
 ((account) @field.account)
 ((commodity) @text.literal.commodity)
+
+((check_in) @text.literal.check_in)
+(check_in . (date) @number.date.check_in)
+(check_in . (date) . (time) @number.time.check_in)
+((check_out) @text.literal.check_out)
+(check_out . (date) @number.date.check_out)
+(check_out . (date) . (time) @number.time.check_out)
 
 "include" @include
 
