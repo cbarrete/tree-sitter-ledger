f7c28b1c11956e7c8c667f18ca25860049bf0df2
Cédric Barreteau <cbarrete@users.noreply.github.com>
2023-04-22 18:27:12 -0400

    Flake: use an explicit nixpkgs input
    Also update the version of tree-sitter while I'm at it.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    devShell.x86_64-linux = pkgs.mkShellNoCC {
+    devShells.x86_64-linux.default = pkgs.mkShellNoCC {
       packages = [ pkgs.tree-sitter pkgs.nodejs ];
     };
   };
